05b7038627a228af0a06eb55ce02ec2beac59eb8
Krithikh <krithikh.g@gmail.com>
2026-02-13 17:30:37 +0800

    THA1A

--- CS2040/THAs/THA1/MountainPeeker.java
+++ CS2040/THAs/THA1/MountainPeeker.java
@@ -1,20 +1,43 @@
+//Krithikh Gopalakrishnan, A0243980Y
 import java.util.*;
 public class MountainPeeker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
-        int height = sc.nextInt();
+        double height = sc.nextInt(); //use double to avoid floating point error
 
         HashMap<Integer, Integer> mountains = new HashMap<>();
-        Integer[] x_filled = new Integer[2*100000 + 1];
-        
+                
         for (int i=0; i<count; i++){
             int curr_x = sc.nextInt();
             int curr_y = sc.nextInt();
             mountains.put(curr_x, curr_y);
-            x_filled[curr_x + 100000] = 1;
         }
 
+        int mountain_count = 0;
+
+        double max_grad = -100000; //iterate through hashmap, for every mountain that exists we check if the gradient is higher than previous highest
+        for (int x=1; x<100001; x++){
+            if(mountains.get(x) != null){
+                double y = mountains.get(x);
+                if((y-height)/x > max_grad){
+                    max_grad = (y-height)/x;
+                    mountain_count ++;
+                }
+            }
+        }
+
+        double min_grad = 100000;
+        for(int x=-1; x>-100001; x--){
+            if(mountains.get(x)!=null){
+                double y = mountains.get(x);
+                if((y-height)/x < min_grad){
+                    min_grad = (y-height)/x;
+                    mountain_count ++;
+                }
+            }
+        }
         
+        System.out.println(mountain_count);
     }
 }
