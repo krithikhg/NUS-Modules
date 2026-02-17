2b88d4c410ec37a54ee304b4b3c6cc0413d5bee1
Krithikh <krithikh.g@gmail.com>
2026-02-17 15:47:59 +0800

    Finished CS2100DE lab 3 tha

--- CS2100DE/Labs/week_5/week_5.srcs/sim_1/new/Counter_sim.sv
+++ CS2100DE/Labs/week_5/week_5.srcs/sim_1/new/Counter_sim.sv
@@ -19,58 +19,55 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module Counter_sim( 
+        ); 
+        
+    logic[7:0] clk; 
+    logic[1:0] rst; 
+    logic[1:0]  up_down; 
+    logic[7:0] count = 0; 
+  
+    Counter uut(clk, rst, up_down, count); 
+    
+    localparam N_COUNTS = (10**8) / (4); //for 4Hz, change denominator according to desired frequency
 
-module Counter_sim(
-        );
-        
-    logic[7:0] clk;
-    logic[1:0] rst;
-    logic[1:0]  up_down;
-    logic[7:0] count;
+    int correct_count = 0; 
+    initial begin 
+        clk = 0; 
+        up_down = 0; 
+        rst = 0; 
+        #1; 
+        rst = 1; 
 
-    
-    Counter uut(clk, rst, up_down, count);
-    
-    int correct_count = 0;
-    
-    initial begin
-        clk = 0;
-        up_down = 0;
-        rst = 0;
-        #1;
-        rst = 1;
-        
-        for(int i=0; i<20; i++) begin
-            if (count != correct_count) begin
-                $error("Counts are not equal");
-                $stop;
-            end
-            
-            else begin
-                correct_count ++;
-                #(10 * 25 * 10**6);
+        for(int i=0; i<20; i++) begin 
+            if (count != correct_count) begin 
+                $error("Counts are not equal"); 
+                $stop; 
             end 
-            
-        end
+            else begin 
+                #(10 * N_COUNTS); 
+                correct_count ++; 
+            end  
+        end 
         
-        up_down = 1;
-        for(int i=0; i<20; i++) begin
-            if (count != correct_count) begin
-                $error("Counts are not equal");
-                $stop;
-            end
-            else begin
-                correct_count --;
-                #(10 * 25 * 10**6);
+        up_down = 1; 
+
+        for(int i=0; i<20; i++) begin 
+            if (count != correct_count) begin 
+                $error("Counts are not equal"); 
+                $stop; 
             end 
-        end
+            else begin 
+                #(10 * N_COUNTS); 
+                correct_count --; 
+            end  
+        end 
     $finish;   
     end 
-    
-    always begin
-        #5;
-        clk = 1;
+    always begin 
         #5; 
-        clk = 0;
-    end
-endmodule
+        clk = 1; 
+        #5;  
+        clk = 0; 
+    end 
+endmodule 
