04fd70a7bb0b79fd6299d8aa4b6f9aa1b4613f93
Krithikh <krithikh.g@gmail.com>
2026-08-31 11:16:28 +0800

    CS2106 Tutorial 2 files

---
+++ CS2106/Tutorials/CS2106Tut2/Code/FF.c
@@ -0,0 +1,24 @@
+/*
+Modified Recursive Factorial for CS2106
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int factorial(int n)
+{
+    if (n == 0){
+        fork();
+        return 1;
+    } 
+
+    return factorial(n-1) * n;
+}
+
+int main()
+{
+    printf("fac(2) = %d\n", factorial(2));
+
+    return 0;
+}
